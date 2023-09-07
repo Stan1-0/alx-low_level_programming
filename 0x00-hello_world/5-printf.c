fdc45a6198f3b96f4bc992cd0e944fb1b6e21b13
Stan1-0 <stankofb@gmail.com>
2023-09-07 15:33:44 -0700

    lets go

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#!/bin/bash <stdio.h>
+/*
+ * main - This program prints a line using the printf function
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
