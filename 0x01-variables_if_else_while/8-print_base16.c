511903c17533e4bf413d1c60c98f55b7f5fbd61c
Stan1-0 <stankofb@gmail.com>
2023-09-08 16:41:43 -0700

    done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,9 +7,13 @@
 int main(void)
 {
 	char i;
-for (i = '0'; i <= '16'; i++)
+for (i = '0'; i <= '9'; i++)
 {
-	putchar(i + '0');
+	putchar(i);
+}
+for (i = 'a'; i <= 'f'; i++)
+{
+	putchar(i);
 }
 putchar('\n');
 return (0);
