7c81ed70b83b8203d54b4c303a84f5cb30cbd705
Stan1-0 <stankofb@gmail.com>
2023-09-12 17:24:32 -0700

    striving

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add -  adds two integers and returns the result
+ * @i: first integer
+ * @n: second integer
+ * Return: 0 (successful)
+ */
+int add(int i, int n)
+{
+	return (i + n);
+}
