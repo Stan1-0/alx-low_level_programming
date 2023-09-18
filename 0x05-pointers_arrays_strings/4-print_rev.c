2b1a14e5e16a65a3ca734412babb7ca98c426beb
Stan1-0 <stankofb@gmail.com>
2023-09-18 15:29:01 -0700

    getting ther

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,26 +6,13 @@
 
 void print_rev(char *s)
 {
-int len = 0, index = 0;
-len = _strlen(s);
+	int c;
 
-for (index = len - 1; index >= 0; index--)
-_putchar(s[index]);
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+	for (c = c - 1; c >= 0; c--)
+		_putchar(s[c]);
+	_putchar('\n');
 
-_putchar('\n');
-}
-
-/**
- *_strlen - returns the length of a string
- * @s: string
- *Return: returns lenght;
- */
-int _strlen(char *s)
-{
-int count, inc;
-inc = 0;
-for (count = 0; s[count] != '\0'; count++)
-inc++;
-
-return (inc);
 }
