23178d530007253f1c2b45253f5488bd3f6bfd53
Stan1-0 <stankofb@gmail.com>
2023-09-25 14:28:08 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_memset - fills memory with a constant
+ *@s:target
+ *@b:constant byte
+ *@n:number of byte
+ *Return: new value of target
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n)
+	{
+		s[n-1] = b;
+		n--;
+	}
+	return (s);
+}
+
